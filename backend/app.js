66a96189f63e49330aa98f8a92e3dd4809ce6daa
seungki <sysh9498@gmail.com>
2024-02-16 16:00:09 +0900

    Chore: Initial Settings

---
+++ backend/app.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const morgan = require("morgan");
+// const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+// const hpp = require("hpp");
+
+//app.js => All about express
+const app = express();
+// SECURITY HTTP HEADER
+app.use(helmet());
+
+// GLOBAL MIDDLEWAREs
+// development logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// FOR BRUTE FORCE ATTACK
+// const limiter = rateLimit({
+//   max: 100,
+//   windowMs: 60 * 60 * 1000,
+//   message: "Too many request from same IP, try later.",
+// });
+// app.use("/api", limiter);
+
+// global middleware, before route handler
+//body parser
+app.use(express.json()); // between req and res
+
+// Data sanitization against NoSQL query Injection
+// email: {$gt: ""} -> 모든 계정 선택됨
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+
+// app.use(
+//   hpp({
+//     // whitelist -> query에서 여러번 나와도 되는 것들 설정
+//     whitelist: [
+//       "duration",
+//       "ratingsAverage",
+//       "ratingsQuantity",
+//       "maxGroupSize",
+//       "difficulty",
+//       "price",
+//     ],
+//   }),
+// );
+
+//serving static files
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// ROUTE HANDLERs
+
+// app.use("/api/v1/tours", tourRouter);
+// app.use("/api/v1/users", userRouter);
+// app.use("/api/v1/reviews", reviewRouter);
+
+// for all http request
+// 여가까지 도달한다면, 앞에서 걸러지지 않았기 때문에 우리가 정의한 페이지가 아님
+// app.all("*", (req, res, next) => {
+// const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+// err.status = 'fail';
+// err.statusCode = 404;
+
+// next 안에 값이 들어간다면 에러로 처리합니다. => 중간의 모든 미들웨어를 스킵
+//   next(new AppError(`Can't find ${req.originalUrl} on this server!`));
+// });
+
+// app.use(globalErrorHandler);
+
+module.exports = app;
---
+++ backend/index.js
@@ -0,0 +1,44 @@
+// const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+
+// 실제 오류가 발생할 곳에서 catch 하는 것이 가장 이상적입니다.
+process.on("uncaughtException", (err) => {
+  console.log("uncaught error ...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+const app = require("./app");
+// const app = require("./starter/app");
+// const Tour = require("./starter/models/tourModel");
+
+// const DB = process.env.DATABASE.replace(
+//   "<PASSWORD>",
+//   process.env.DATABASE_PASSWORD,
+// );
+
+// mongoose
+//   .connect(DB, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true,
+//   })
+//   .then((con) => {
+//     console.log("DB connection successful!");
+//   });
+
+const port = 8000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("Unhandled rejection...");
+  // 0 > ok, 1 > on error
+  // 바로 프로세스를 종료하는 것이 아닌, 서버에서 펜딩중이나 작업중인 작업을 모두 마친 뒤 종료해야.
+  process.exit(1);
+});
