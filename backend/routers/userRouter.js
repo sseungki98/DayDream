8cbdd362dc3bded31472174635568f6b953bf1a8
seungki <sysh9498@gmail.com>
2024-02-17 17:59:04 +0900

    Feat: 구현한 기능을 라우터에 추가

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -4,6 +4,30 @@ const authController = require("../Controllers/authController");
 
 const router = express.Router();
 
-router.route("/").get(userController.getAllUser).post(authController.signup);
+router.route("/signup").post(authController.signup);
+router.route("/login").get(authController.login);
+
+router.route("/").get(userController.getAllUser);
+router
+  .route("/updateMe")
+  .patch(authController.protect, userController.updateMe);
+
+router.route("/me").get(authController.protect, userController.getMe);
+
+router
+  .route("/todayMatch")
+  .get(
+    authController.protect,
+    authController.restritTo("user"),
+    userController.getTodayMatchUsers,
+  );
+
+router
+  .route("/:id")
+  .get(
+    authController.protect,
+    authController.restritTo("admin"),
+    userController.getOneUser,
+  );
 
 module.exports = router;
