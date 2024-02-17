c1c26bcc2bf826e953c9f6ffe6b05156cfeab821
seungki <sysh9498@gmail.com>
2024-02-17 17:57:09 +0900

    Feat: getTodayMatchUsers, getMe, getOneUser 구현 getTodayMatchUsers -> 랜덤으로 20명의 유저를 가져옴 getMe -> 현재 유저 정보 반환 getOneUser -> 한명의 유저 정보를 반환

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/userModel");
+const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 
 exports.getAllUser = catchAsync(async (req, res, next) => {
@@ -16,6 +17,61 @@ exports.createUser = catchAsync(async (req, res, next) => {
   const user = await User.create(req.body);
 
   res.status(201).json({
+    status: "success",
+    data: {
+      data: user,
+    },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  const user = await User.findByIdAndUpdate(req.user.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!user) {
+    return next(new AppError("유효한 아이디가 아닙니다."), 404);
+  }
+});
+
+exports.getOneUser = catchAsync(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+  if (!user) {
+    return next(new AppError("존재하지 않는 유저입니다.", 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      data: user,
+    },
+  });
+});
+
+exports.getTodayMatchUsers = catchAsync(async (req, res, next) => {
+  const users = await User.aggregate([
+    {
+      $match: { active: true, role: "user" },
+    },
+    {
+      $sample: { size: 20 },
+    },
+  ]);
+
+  res.status(200).json({
+    status: "success",
+    results: users.length,
+    data: {
+      data: users,
+    },
+  });
+});
+
+exports.getMe = catchAsync(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({
     status: "success",
     data: {
       data: user,
