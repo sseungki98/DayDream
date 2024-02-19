135e2b96948459c9d407a0d96e76c0fc65683d79
seungki <sysh9498@gmail.com>
2024-02-19 15:51:44 +0900

    Feat: 인덱스 페이지 구현

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -1,124 +1,49 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
-import Input from "../components/Input";
-import { userInfo } from "os";
-import api from "../apis/api";
+import { useNavigate } from "react-router-dom";
+import { Background } from "./SignUp";
+import ThroughButton from "../components/ThroughButton";
 
 function Index() {
-  const [signupInfo, setSignupInfo] = useState({
-    email: "",
-    password: "",
-    passwordConfirm: "",
-    name: "",
-    introduce: "",
-    age: 0,
-    height: 0,
-    weight: 0,
-  });
-  const [page, setPage] = useState<Number>(0);
-  const handlePageMove = () => {
-    const { email, password, passwordConfirm } = signupInfo;
-    if (!email || !password || !passwordConfirm) {
-      alert("모두 채워주세요.");
-    } else if (password !== passwordConfirm) {
-      alert("동일한 비밀번호를 입력해주세요.");
-    } else {
-      setPage(1);
+  const navigate = useNavigate();
+  const navigator = (url: string) => {
+    if (url === "/signup") {
+      navigate("/signup");
+    } else if (url === "/login") {
+      navigate("/login");
     }
   };
-  const FirstPageInputs = () => {
-    return (
-      <>
-        <Input
-          type="text"
-          value={signupInfo.email}
-          placeholder="이메일"
-          onChange={ev => setSignupInfo(userInfo => ({ ...userInfo, email: ev.target.value }))}
-        />
-        <Input
-          type="text"
-          value={signupInfo.password}
-          placeholder="비밀번호"
-          onChange={ev => setSignupInfo(userInfo => ({ ...userInfo, password: ev.target.value }))}
-        />
-        <Input
-          type="text"
-          value={signupInfo.passwordConfirm}
-          placeholder="비밀번호 확인"
-          onChange={ev => setSignupInfo(userInfo => ({ ...userInfo, passwordConfirm: ev.target.value }))}
-        />
-        <Button onClick={handlePageMove}>가입하기</Button>
-      </>
-    );
-  };
-
-  const SecondPageInputs = () => {
-    return (
-      <>
-        <Input
-          type="text"
-          value={signupInfo.name}
-          placeholder="닉네임"
-          onChange={ev => setSignupInfo(userInfo => ({ ...userInfo, name: ev.target.value }))}
-        />
-        <Input
-          type="text"
-          value={signupInfo.introduce}
-          placeholder="자기소개"
-          onChange={ev => setSignupInfo(userInfo => ({ ...userInfo, introduce: ev.target.value }))}
-        />
-        <Input
-          type="number"
-          value={signupInfo.age}
-          placeholder="나이"
-          onChange={ev => setSignupInfo(userInfo => ({ ...userInfo, age: Number(ev.target.value) }))}
-        />
-        <Input
-          type="number"
-          value={signupInfo.height}
-          placeholder="키"
-          onChange={ev => setSignupInfo(userInfo => ({ ...userInfo, height: Number(ev.target.value) }))}
-        />
-        <Input
-          type="number"
-          value={signupInfo.weight}
-          placeholder="몸무게"
-          onChange={ev => setSignupInfo(userInfo => ({ ...userInfo, weight: Number(ev.target.value) }))}
-        />
-        <Button type="submit">가입하기</Button>
-      </>
-    );
-  };
-
-  const handleSubmit = () => {
-    api
-      .post("/users/signup", signupInfo)
-      .then(res => console.log(res))
-      .catch(err => console.log(err));
-  };
-
   return (
     <Background>
-      <form onSubmit={handleSubmit}>{page === 0 ? FirstPageInputs() : SecondPageInputs()}</form>
+      <Logo className="super-magic">DayDream</Logo>
+      <ButtonContainer>
+        <ThroughButton message="로그인하기" onClick={() => navigator("/login")} />
+        <ThroughButton message="가입하기" onClick={() => navigator("/signup")} />
+        {/* <Button onClick={() => navigator("/login")}>로그인하기</Button>
+        <Button onClick={() => navigator("/signup")}>가입하기</Button> */}
+      </ButtonContainer>
     </Background>
   );
 }
 
-export default Index;
-
-const Background = styled.div`
-  background-color: #bacfff;
-  height: 100vh;
+export const Logo = styled.p`
+  font-size: 60px;
+  margin-top: 0px;
+  text-align: center;
 `;
 
-const Button = styled.button`
-  width: 82.5%;
-  height: 45px;
-  border-radius: 10px;
-  border: 1px solid #86a9f9;
-  background-color: #f7f7f7;
-  cursor: pointer;
-  &:hover {
-    background-color: #baddf5;
+export const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 30px;
+  form {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 30px;
   }
 `;
+
+export default Index;
