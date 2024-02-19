fc733604c7eaa05ef5e55bcef6040d5377139f3e
seungki <sysh9498@gmail.com>
2024-02-19 15:50:21 +0900

    Feat: 라우터 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import logo from "./logo.svg";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import IndexPage from "./pages/Index";
+import Index from "./pages/Index";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" Component={IndexPage} />
+        <Route path="/" Component={Index} />
+        <Route path="/signup" Component={SignUp} />
+        <Route path="/login" Component={Login} />
       </Routes>
     </BrowserRouter>
   );
