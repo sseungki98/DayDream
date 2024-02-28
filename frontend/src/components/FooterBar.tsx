3936fae62b12eaa1579c483b1d2c60d5d2e18c7a
seungki <sysh9498@gmail.com>
2024-02-28 16:32:13 +0900

    Fix: 하단바 페이지 이동 방식 변경

--- frontend/src/components/FooterBar.tsx
+++ frontend/src/components/FooterBar.tsx
@@ -1,29 +1,51 @@
 import React from "react";
 import styled from "styled-components";
 import { FooterType } from "../pages/Main";
+import { useNavigate } from "react-router-dom";
 
 export const IconRoute = (icon: string) => {
   return process.env.PUBLIC_URL + "/img/" + icon + "-icon.svg";
 };
 
-function FooterBar({ onItemClick }: { onItemClick: (item: FooterType) => void }) {
-  const clickHandler = (e: React.MouseEvent<HTMLDivElement>) => {
+// { onItemClick }: { onItemClick: (item: FooterType) => void }
+function FooterBar() {
+  const navigator = useNavigate();
+  // const clickHandler = (e: React.MouseEvent<HTMLDivElement>) => {
+  //   const target = e.target as HTMLButtonElement;
+  //   onItemClick(target.value as FooterType);
+  // };
+  const navigateHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
     const target = e.target as HTMLButtonElement;
-    onItemClick(target.value as FooterType);
+    const route = target.value;
+    navigator(`/${route}`);
   };
 
   return (
-    <FooterContainer onClick={e => clickHandler(e)}>
-      <FooterItem value="lookup" key="lookup" className="item">
+    // <FooterContainer onClick={e => clickHandler(e)}>
+    //   <FooterItem value="lookup" key="lookup" className="item">
+    //     <img src={IconRoute("lookup")} alt="lookup-icon" width={48} height={48} />
+    //   </FooterItem>
+    //   <FooterItem value="chat" key="chat" className="item">
+    //     <img src={IconRoute("chat")} alt="chat-icon" width={48} height={48} />
+    //   </FooterItem>
+    //   <FooterItem value="etc" key="etc" className="item">
+    //     <img src={IconRoute("profile")} alt="profile-icon" width={48} height={48} />
+    //   </FooterItem>
+    //   <FooterItem value="mypage" key="mypage" className="item">
+    //     <img src={IconRoute("profile")} alt="profile-icon" width={48} height={48} />
+    //   </FooterItem>
+    // </FooterContainer>
+    <FooterContainer>
+      <FooterItem value="lookup" key="lookup" className="item" onClick={e => navigateHandler(e)}>
         <img src={IconRoute("lookup")} alt="lookup-icon" width={48} height={48} />
       </FooterItem>
-      <FooterItem value="chat" key="chat" className="item">
+      <FooterItem value="chat" key="chat" className="item" onClick={e => navigateHandler(e)}>
         <img src={IconRoute("chat")} alt="chat-icon" width={48} height={48} />
       </FooterItem>
-      <FooterItem value="etc" key="etc" className="item">
+      <FooterItem value="etc" key="etc" className="item" onClick={e => navigateHandler(e)}>
         <img src={IconRoute("profile")} alt="profile-icon" width={48} height={48} />
       </FooterItem>
-      <FooterItem value="mypage" key="mypage" className="item">
+      <FooterItem value="mypage" key="mypage" className="item" onClick={e => navigateHandler(e)}>
         <img src={IconRoute("profile")} alt="profile-icon" width={48} height={48} />
       </FooterItem>
     </FooterContainer>
