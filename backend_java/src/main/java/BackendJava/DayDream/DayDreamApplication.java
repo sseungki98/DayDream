9da1faed9120e5211490b7e6fb57d802abcf1a5c
Hongjun <ggaye159@naver.com>
2024-02-29 12:35:25 +0900

    clear git cache

---
+++ backend_java/src/main/java/BackendJava/DayDream/DayDreamApplication.java
@@ -0,0 +1,13 @@
+package BackendJava.DayDream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayDreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayDreamApplication.class, args);
+	}
+
+}
