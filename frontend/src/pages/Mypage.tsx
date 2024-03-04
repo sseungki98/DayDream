f017e81bf83655aa749e074bc20d90a91e72be80
seungki <sysh9498@gmail.com>
2024-03-05 01:48:30 +0900

    Feat: 마이페이지 임시 구현

--- frontend/src/pages/Mypage.tsx
+++ frontend/src/pages/Mypage.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { Background } from "./SignUp";
+import FooterBar from "../components/FooterBar";
 
 function Mypage() {
-  return <Background>마이페이지</Background>;
+  return (
+    <Background>
+      <p>마이페이지</p>
+      <FooterBar />
+    </Background>
+  );
 }
 
 export default Mypage;
